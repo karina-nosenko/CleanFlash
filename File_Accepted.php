e054d2d868a6bd6aca2c73a5edbe4c17f64f09df
karina <karina4nosenko@gmail.com>
2021-05-22 22:38:59 +0300

    changed the wrapper properties

--- File_Accepted.php
+++ File_Accepted.php
@@ -125,8 +125,8 @@
 
         <!-- MAIN -->
         <main>
-            <header>
-            </header>
+            <section class="header">
+            </section>
 
             <section id="content">
                 <h5> 
